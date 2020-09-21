95d62a24323753cd81bf30be07afd5d3bca5505e
olzhas <toleugazin0@gmail.com>
2020-09-21 22:14:52 +0600

    Initial Commit

---
+++ Movie App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie App
+//
+//  Created by Olzhas Toleugazin on 9/21/20.
+//  Copyright © 2020 Olzhas Toleugazin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
